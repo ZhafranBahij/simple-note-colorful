69524df73adc9386ad004af34df735f7d9ee84f9
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2025-04-28 13:07:54 +0700

    feat: install breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'kirakira4141',
+            'email' => 'kirakira4141@example.com',
+            'password' => 'kirakira4141',
         ]);
     }
 }
